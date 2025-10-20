243018c151f9bb6021c2e5cdb97e14b3f9ac0315
projaska <laurava.coreico@gmail.com>
2025-10-20 19:50:36 -0300

    terminando seccion contacto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./componentes/layeut/Header";
 import Hero from "./componentes/layeut/Hero";
 import Trabajos from  "./componentes/layeut/Trabajos";
-import AcercaDe from "./componentes/layeut/Acercade";
+import AcercaDe from "./componentes/layeut/AcercaDe";
 import Contacto from "./componentes/layeut/Contacto";
 
 function App() {
